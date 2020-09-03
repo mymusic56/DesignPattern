8003c167e390d2ea098719d8a9b7dbc785e9f0f3
zhangshengji <961272668@qq.com>
2020-09-03 23:08:28 +0800

    scan file

---
+++ src/main/java/com/zsj/designpattern/composite/FileNode.java
@@ -0,0 +1,138 @@
+package com.zsj.designpattern.composite;
+
+import javax.xml.soap.Node;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+enum NodeType {
+    FILE,
+    DIR;
+}
+
+class Test {
+    public static void main(String[] args) {
+        String path = "D:\\workspace-Java\\my_project\\DesignPattern\\src\\main\\java\\com\\zsj\\designpattern\\demo";
+        File file = new File(path);
+        System.out.println(file.getAbsolutePath());
+        FileNode node = new FileNode(file.getAbsolutePath(), file.getName(), NodeType.DIR);
+
+        Test test = new Test();
+        test.scanFileOne(node, file);
+
+        test.print(node);
+    }
+
+    private void print(FileNode node) {
+        for (FileNode n : node.getFileNodeList()) {
+            System.out.println(n.getPath());
+            if (n.getNodeType().equals(NodeType.DIR)) {
+                print(n);
+            }
+        }
+    }
+
+    private void printV2(FileNode node) {
+        for (FileNode n : node.getFileNodeList()) {
+            System.out.println(n.getPath());
+            if (n.getNodeType().equals(NodeType.DIR)) {
+                print(n);
+            }
+        }
+    }
+
+    private void scanFileOne(FileNode node, File file) {
+
+        if (file.exists()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    if (f.isDirectory()) {
+                        FileNode tmpNode = new FileNode(f.getAbsolutePath(), f.getName(), NodeType.DIR);
+                        node.addNode(tmpNode);
+                        scanFileOne(tmpNode, f);
+                    }
+                    node.addNode(new FileNode(f.getAbsolutePath(),f.getName(), NodeType.FILE));
+                }
+            }
+        }
+    }
+
+}
+
+public class FileNode {
+    private FileNode parentNode = null;
+
+    private String path;
+
+    private String name;
+
+    private NodeType nodeType;
+
+    private long fileSize = 0L;
+
+    private long fileCount = 0L;
+
+    //子节点
+    private List<FileNode> fileNodeList = new ArrayList<>();
+
+    public FileNode(String path, String filename, NodeType nodeType) {
+        this.path = path;
+        this.nodeType = nodeType;
+        this.name = filename;
+    }
+
+    public void addNode(FileNode node) {
+        node.parentNode = this;
+        fileNodeList.add(node);
+        countOfFiles();
+        sizeOfFiles();
+    }
+
+    public void removeNode(FileNode node) {
+        FileNode parentNode = node.getParentNode();
+        parentNode.getFileNodeList().remove(node);
+    }
+
+    /**
+     * 计算该节点文件数量
+     */
+    private void countOfFiles() {
+
+    }
+
+    /**
+     * 计算该节点文件大小
+     */
+    private void sizeOfFiles() {
+
+    }
+
+    public FileNode getParentNode() {
+        return parentNode;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public NodeType getNodeType() {
+        return nodeType;
+    }
+
+    public List<FileNode> getFileNodeList() {
+        return fileNodeList;
+    }
+
+    public long getFileSize() {
+        return fileSize;
+    }
+
+    public long getFileCount() {
+        return fileCount;
+    }
+}
